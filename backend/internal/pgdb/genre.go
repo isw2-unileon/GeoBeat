c0810dd5f9b6c5635031fa616a9b70eb99fc60bf
IvanAntequera <ianteb00@estudiantes.unileon.es>
2026-05-01 18:21:53 +0200

    create genre repository logic

---
+++ backend/internal/pgdb/genre.go
@@ -0,0 +1,38 @@
+package pgdb
+
+import (
+	"context"
+
+	"github.com/isw2-unileon/GeoBeat/backend/internal/genre"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type PostgresGenreRepo struct {
+	pool *pgxpool.Pool
+}
+
+func NewPostgresGenreRepo(pool *pgxpool.Pool) *PostgresGenreRepo {
+	return &PostgresGenreRepo{pool: pool}
+}
+
+// GetAllowedGenres retrieves the list of allowed genres from the database.
+func (r *PostgresGenreRepo) GetAllowedGenres(ctx context.Context) ([]genre.Genre, error) {
+	rows, err := r.pool.Query(ctx, "SELECT id, name, normalized_name FROM genres")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var genres []genre.Genre
+	for rows.Next() {
+		var genre genre.Genre
+		if err := rows.Scan(&genre.ID, &genre.Name, &genre.NormalizedName); err != nil {
+			return nil, err
+		}
+		genres = append(genres, genre)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return genres, nil
+}
