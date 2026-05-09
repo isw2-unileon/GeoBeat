e40c1d0a7d8d713ecd9f18624cdb6b844afd65e5
IvanAntequera <ianteb00@estudiantes.unileon.es>
2026-05-09 08:52:55 +0200

    genre interface moved to where it is consumed

--- backend/internal/genre/genre.go
+++ backend/internal/genre/genre.go
@@ -1,15 +1,8 @@
 package genre
 
-import "context"
-
 // Genre represents a music genre with its ID, name, and normalized name.
 type Genre struct {
 	ID             int    `json:"id"`
 	Name           string `json:"name"`
 	NormalizedName string `json:"normalized_name"`
-}
-
-// Repository defines the methods required to manage genres.
-type Repository interface {
-	GetAllowedGenres(ctx context.Context) ([]Genre, error)
 }
