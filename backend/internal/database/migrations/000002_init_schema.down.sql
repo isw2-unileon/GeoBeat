ece3f635eeb3a278b248e073aee9ee251ece8fdf
IvanAntequera <ianteb00@estudiantes.unileon.es>
2026-06-04 13:08:08 +0200

    refactor name migrations

--- backend/internal/database/migrations/000002_init_schema.down.sql
+++
@@ -1,3 +0,0 @@
-DROP INDEX IF EXISTS idx_timeattack_leaderboard;
-DROP TABLE IF EXISTS timeattack_sessions;
-DROP TABLE IF EXISTS timeattack_challenges;
--- backend/internal/database/migrations/000002_timetrial_schema.up.sql
+++ backend/internal/database/migrations/000002_timetrial_schema.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE timeattack_challenges (
+CREATE TABLE timetrial_challenges (
     id SERIAL PRIMARY KEY,
     target_countries TEXT[] NOT NULL,
     target_genres TEXT[] NOT NULL,
@@ -7,9 +7,9 @@ CREATE TABLE timeattack_challenges (
     CONSTRAINT arrays_length_match CHECK (array_length(target_countries, 1) = array_length(target_genres, 1))
 );
 
-CREATE TABLE timeattack_sessions (
+CREATE TABLE timetrial_sessions (
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    challenge_id INT NOT NULL REFERENCES timeattack_challenges(id) ON DELETE CASCADE,
+    challenge_id INT NOT NULL REFERENCES timetrial_challenges(id) ON DELETE CASCADE,
     current_index INT NOT NULL DEFAULT 0,
     start_time TIMESTAMP WITH TIME ZONE NOT NULL,
     end_time TIMESTAMP WITH TIME ZONE,
@@ -20,6 +20,6 @@ CREATE TABLE timeattack_sessions (
     CONSTRAINT valid_status CHECK (status IN ('playing', 'completed'))
 );
 
-CREATE INDEX idx_timeattack_leaderboard 
-ON timeattack_sessions (challenge_id, duration ASC) 
+CREATE INDEX idx_timetrial_leaderboard 
+ON timetrial_sessions (challenge_id, duration ASC) 
 WHERE status = 'completed';
