94a9d81965949b2f8f2d3908208db456ef0d1114
dportc01 <dportc01@estudiantes.unileon.es>
2026-06-05 19:43:59 +0200

    Fix typo

--- frontend/src/components/inverse/inverse-title.tsx
+++ frontend/src/components/inverse/inverse-title.tsx
@@ -5,7 +5,7 @@ export function InverseModeTitle() {
                 absolute top-2 left-1/2 -translate-x-1/2 text-outline
                 text-2xl text-center text-orange-600 font-semibold font-[sans] animate-fade-in-down z-1"
     >
-      GUESS THE COUNRTY
+      GUESS THE COUNTRY
     </h1>
   );
 }
